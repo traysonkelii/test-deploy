5eb64b7664d0a365065532156e20a61c349b5e43
traysonkelii <traysonkelii@gmail.com>
2022-07-31 19:03:52 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Mastering the damn CICD
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          WHOOO HOO
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
